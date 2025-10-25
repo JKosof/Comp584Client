d969a9a6765c3718b88d4a999fec42f04c8095a5
DragonStorm247 <joshkosof@gmail.com>
2025-10-24 17:39:10 -0700

    country population

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopulationData } from './population-data';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss'
 })
-export class CountryPopulation {
-  country!: PopulationData;
-    constructor(http: HttpClient){
-      http.get<PopulationData>(environment.apiUrl + '/api/Countries/population/26').subscribe(result => {
-        this.country = result;
-      });
+export class CountryPopulation implements OnInit {
+  countryPopulation!: PopulationData;
+    constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+      
     }
+
+  ngOnInit(): void {
+    let idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get<PopulationData>(`${environment.apiUrl}/api/Countries/population/${idParam}`).subscribe(result => {
+        this.countryPopulation = result;
+      });
+  }
 }
